6630edf8bfc73330939d2f696907ae61f6478864
VanessaLin9 <vanessa7591@gmail.com>
2022-08-21 03:25:28 -0400

    feat: conver temperature

---
+++ src/component/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Conversion from './conversion';
+
+const App = () => {
+  const [inputF, setInputF] = React.useState('');
+  const [inputC, setInputC] = React.useState('');
+  const [holderF, setHolderF] = React.useState('320');
+  const [holderC, setHolderC] = React.useState('160');
+
+  const atChange = (e) => {
+    if (e.target.id === 'inputF') {
+      setInputF(e.target.value);
+    } else if (e.target.id === 'inputC') {
+      setInputC(e.target.value);
+    }
+  };
+  const atClick = (e) => {
+    if (e.target.id === 'inputF') {
+      setInputC('');
+    } else if (e.target.id === 'inputC') {
+      setInputF('');
+    }
+  };
+
+  React.useEffect(() => {
+    let converC = ((inputF - 32) * 5) / 9;
+    if (converC % 1 !== 0) {
+      converC = converC.toFixed(4);
+    }
+    setHolderC(converC);
+  }, [inputF]);
+
+  React.useEffect(() => {
+    let converF = (inputC * 9) / 5 + 32;
+    if (converF % 1 !== 0) {
+      converF = converF.toFixed(4);
+    }
+    setHolderF(converF);
+  }, [inputC]);
+
+  return (
+    <div className="App">
+      <Conversion
+        inputF={inputF}
+        inputC={inputC}
+        holderF={holderF}
+        holderC={holderC}
+        onChange={atChange}
+        onClick={(e) => atClick(e)}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/conversion/index.jsx
@@ -0,0 +1,52 @@
+// import React, { useEffect } from 'react';
+import './Conversion.css';
+
+type ConversionProps = {
+  inputF: number,
+  inputC: number,
+  holderF: number,
+  holderC: number,
+  onChange: (e) => void,
+  onClick: (e) => void,
+};
+
+const Conversion: React.FC<ConversionProps> = (props) => {
+  const { inputF, inputC, holderF, holderC, onChange, onClick } = props;
+
+  return (
+    <section>
+      <div className="container">
+        {/* <form> */}
+        <label className="inputLabel">
+          <input
+            id="inputF"
+            type="number"
+            className="my-input"
+            placeholder={holderF}
+            value={inputF}
+            onChange={onChange}
+            onClick={onClick}
+            // disabled={disableF}
+          />
+          &deg;F
+        </label>
+        <label className="inputLabel">
+          <input
+            id="inputC"
+            type="number"
+            className="my-input"
+            placeholder={holderC}
+            value={inputC}
+            onChange={onChange}
+            onClick={onClick}
+            // disabled={disableC}
+          />
+          &deg;C
+        </label>
+        {/* </form> */}
+      </div>
+    </section>
+  );
+};
+
+export default Conversion;
